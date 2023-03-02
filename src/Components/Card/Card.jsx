16ee48ac5363a81a7cdcec5ca13395f14f14918b
ory-code <70527030+ory-code@users.noreply.github.com>
2023-03-02 15:50:16 +0100

    clear code

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,13 +1,6 @@
 import React from "react";
 import "./Card.scss";
-// import Card from "@mui/material/Card";
-// import CardContent from "@mui/material/CardContent";
-// import CardMedia from "@mui/material/CardMedia";
-// import Typography from "@mui/material/Typography";
-// import { CardActionArea } from "@mui/material";
-import varius from "../../Assets/Images/varius.png";
-import euTempure from "../../Assets/Images/eu-tempus.png";
-import blandit from "../../Assets/Images/blandit.png";
+
 import { Button } from "@mui/material";
 const Cards = (props) => {
   return (
@@ -25,26 +18,6 @@ const Cards = (props) => {
           </div>
         </div>
       </div>
-
-      {/* <Card sx={{ maxWidth: 345, padding: 2.3 }}>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            height="294"
-            image={varius}
-            alt="green iguana"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h3" component="div">
-              Lizard
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Lizards are a widespread group of squamate reptiles, with over
-              6,000 species, ranging across all continents except Antarctica
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card> */}
     </div>
   );
 };
